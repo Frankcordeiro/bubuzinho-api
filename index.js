2d86bccba2892a1caf17ae466e271b19983e6c63
Frankcordeiro <frank.bubure.altamira@gmail.com>
2025-04-12 00:50:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.post('/mensagem', (req, res) => {
-    const pergunta = req.body.mensagem;
-    console.log('Mensagem recebida:', pergunta);
-
-    const resposta = `Bubuzinho diz: você perguntou: ${pergunta}`;
-    return res.json({ resposta });
-});
-
-const PORTA = process.env.PORT || 8080;
-app.listen(PORTA, () => {
-    console.log(`Rodando na porta ${PORTA}`);
-});
